d2fc73de7fab5661362bbea4ca88d400a3c90b40
a.hoseni <a.hoseni@DPK-115>
2024-05-05 13:48:14 +0330

    Add project files.

---
+++ ASPNetIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNetIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
