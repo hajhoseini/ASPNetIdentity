498e24adf07c73765a0103e5c17536977dbf5a9b
a.hoseni <a.hoseni@DPK-115>
2024-05-05 16:08:59 +0330

    getlist users

---
+++ ASPNetIdentity/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using ASPNetIdentity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNetIdentity.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_userManager.Users.ToList());
+        }
+    }
+}
