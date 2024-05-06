f53fe085e168b270a1039e87d527caf5c6bfd830
a.hoseni <a.hoseni@DPK-115>
2024-05-06 15:55:47 +0330

    Login

---
+++ ASPNetIdentity/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using ASPNetIdentity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNetIdentity.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Login2(string email, string password)
+        {
+            var user = _userManager.FindByEmailAsync(email).Result;
+            if(user != null)
+            {
+                var test = _signInManager.SignOutAsync();
+                var result = _signInManager.PasswordSignInAsync(user, password, false, false).Result;
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Admin");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ ASPNetIdentity/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNetIdentity.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+            //https://localhost:7148/Account/Login?ReturnUrl=%2Fadmin%2Findex
+        }
+    }
+}
